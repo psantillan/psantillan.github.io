25aa9f79c2da5b9e91f80087296d3affc5cfb1e3
Paul Santillan <bamf@Pauls-MBP.lan>
2019-02-24 14:57:26 -0800

    Initial commit

---
+++ precache-manifest.f678460ee7360072257066c36e23e96f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c5abc57d9ffc5415a3d",
+    "url": "/static/css/main.08ad853b.chunk.css"
+  },
+  {
+    "revision": "1c5abc57d9ffc5415a3d",
+    "url": "/static/js/main.1c5abc57.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "044ffb9aeb2d8e747ff1",
+    "url": "/static/js/2.044ffb9a.chunk.js"
+  },
+  {
+    "revision": "d8de6b7d94944bd9832e40c765b13686",
+    "url": "/static/media/JUMP_Bleed.d8de6b7d.svg"
+  },
+  {
+    "revision": "39521d31c4a5a78c73bb914af5055ff7",
+    "url": "/index.html"
+  }
+];
