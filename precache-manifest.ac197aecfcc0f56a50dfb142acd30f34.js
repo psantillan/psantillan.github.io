1aef2a176cf95e70c0c7f06164146799756ddaec
Paul Santillan <bamf@Pauls-MBP.local.tld>
2019-02-26 11:20:58 -0800

    second pages commit

---
+++ precache-manifest.ac197aecfcc0f56a50dfb142acd30f34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f184210e8f16cdfbaa48",
+    "url": "/static/css/main.a643c979.chunk.css"
+  },
+  {
+    "revision": "f184210e8f16cdfbaa48",
+    "url": "/static/js/main.f184210e.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "856e4feaae6ca78cc8fd",
+    "url": "/static/js/2.856e4fea.chunk.js"
+  },
+  {
+    "revision": "d8de6b7d94944bd9832e40c765b13686",
+    "url": "/static/media/JUMP_Bleed.d8de6b7d.svg"
+  },
+  {
+    "revision": "fedb1f8915cba7421660b69edfb84e8e",
+    "url": "/index.html"
+  }
+];
